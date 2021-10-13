a4f9dd8add378eec4a539d80c20504d67ba2322d
yehuda dales <yda770@gmail.com>
2021-10-13 14:58:03 +0300

    Add project files.

---
+++ GetPdfPagesCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.IO;
+
+namespace GetPdfPagesCount
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                return -1;
+            }
+
+            
+            var doc = new PdfDocument(new PdfReader(args[0].Trim()));
+            Type type = doc.GetType();
+
+            string[] lines =
+            {            
+                doc.GetNumberOfPages().ToString()             
+            };
+
+            File.WriteAllLinesAsync(args[1].Trim(), lines);
+
+            doc.Close();
+            return 0;
+
+        }
+    }
+}
